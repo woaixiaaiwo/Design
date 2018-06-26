b49edc28a7aa89d63fb339e72c16acbe93b6774f
woaixiaaiwo <877033250@qq.com>
2018-06-26 22:45:39 +0800

    添加计数基数排序

---
+++ src/main/java/com/boge/algorithm/sorts/sortclasses/JiShuPaiXu.java
@@ -0,0 +1,49 @@
+package com.boge.algorithm.sorts.sortclasses;
+
+import java.util.Arrays;
+
+public class JiShuPaiXu {
+
+	public static void main(String[] args) {
+		
+		String[] arr = {"012","021","120","210","201"};
+		sort(arr,3);
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	private static void sort(String[] arr,int length){
+		
+		int N = 60;
+		int k;
+		String[] buffer = new String[arr.length];
+		
+		int count[] = new int[N];
+		int offset[] = new int[N];
+		
+		for(int i=length-1;i>=0;i--){
+			for(int j=0;j<arr.length;j++){
+				//生成count数组
+				count[arr[j].charAt(i)]++;
+			}
+			//根据count数组，生成offset数组
+			for(k=1;k<offset.length;k++){
+				offset[k] = count[k]+offset[k-1];
+				offset[k-1] = offset[k-1] - count[k-1];
+			}
+			offset[k-1] = offset[k-1] - count[k-1];
+			
+			for(int j=0;j<arr.length;j++){
+				//设置数据
+				if(count[arr[j].charAt(i)] > 0){
+					buffer[offset[arr[j].charAt(i)]++] = arr[j];
+					count[arr[j].charAt(i)]--;
+				}
+			}
+			System.arraycopy(buffer,0, arr, 0, buffer.length);
+			count = new int[N];
+			offset = new int[N];
+		}
+		
+	}
+	
+}
